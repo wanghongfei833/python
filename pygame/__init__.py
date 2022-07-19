d0f76600dc0d53681fdf5b5a5de330fdef0f836c
hongfei <1009503644@qq.com>
2022-07-19 13:58:16 +0800

    默认变更列表

---
+++ pygame/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/19 12:03
+# @Author  : HongFei Wang
---
+++ pygame/screen.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/19 12:04
+# @Author  : HongFei Wang
+import pygame
